c57468d0a668e05daa11d5d59d3a4e8c932c9139
xaviSalazar <xavicoel@gmail.com>
2024-11-29 08:40:45 +0100

    Added show text

--- nodes.py
+++ nodes.py
@@ -191,11 +191,55 @@ class ImageToBase64:
             img.save(file_path, pnginfo=metadata, compress_level=self.compress_level)
 
         return (results,)
+
+class ShowText:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "text": ("STRING", {"forceInput": True}),
+            },
+            "hidden": {
+                "unique_id": "UNIQUE_ID",
+                "extra_pnginfo": "EXTRA_PNGINFO",
+            },
+        }
+
+    INPUT_IS_LIST = True
+    RETURN_TYPES = ("STRING",)
+    FUNCTION = "notify"
+    OUTPUT_NODE = True
+    OUTPUT_IS_LIST = (True,)
+
+    CATEGORY = "utils"
+
+    def notify(self, text, unique_id=None, extra_pnginfo=None):
+        if unique_id is not None and extra_pnginfo is not None:
+            if not isinstance(extra_pnginfo, list):
+                print("Error: extra_pnginfo is not a list")
+            elif (
+                not isinstance(extra_pnginfo[0], dict)
+                or "workflow" not in extra_pnginfo[0]
+            ):
+                print("Error: extra_pnginfo[0] is not a dict or missing 'workflow' key")
+            else:
+                workflow = extra_pnginfo[0]["workflow"]
+                node = next(
+                    (x for x in workflow["nodes"] if str(x["id"]) == str(unique_id[0])),
+                    None,
+                )
+                if node:
+                    node["widgets_values"] = [text]
+
+        return {"ui": {"text": text}, "result": (text,)}
+
 # Register the node class with ComfyUI
 NODE_CLASS_MAPPINGS = {
     "LoadBase64Image": LoadBase64Image,
     "LoadBase64Mask": LoadBase64Mask,
-    "ImageToBase64": ImageToBase64
+    "ImageToBase64": ImageToBase64,
+    "ShowText": ShowText,
+
 
 }
 
@@ -203,5 +247,6 @@ NODE_CLASS_MAPPINGS = {
 NODE_DISPLAY_NAME_MAPPINGS = {
     "LoadBase64Image": "Load Base64 Image Node",
     "LoadBase64Mask": "Load Base64 Mask Node",
-    "ImageToBase64": "Image to Base64 Node"
+    "ImageToBase64": "Image to Base64 Node",
+    "ShowText": "Show Text",
 }
